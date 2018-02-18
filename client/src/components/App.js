44f75f58d701603151bc92cf92315ce076b33c8c
Carlos Chacin <chacin.carlos9798@gmail.com>
2018-02-18 04:58:53 -0500

    Added logo and animation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import logo from '../logo_black_green.svg'
-import '../styles/App.css';
+import '../styles/Logo_effect.css';
 
 const App = () => (
   <div>
-    <img id='logo' src={logo} />
+    <img id='logo' alt='Joule Logo' src={logo} />
   </div>
 )
 
