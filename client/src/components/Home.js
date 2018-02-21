3d568a1163683b48025d1d500dbafa44d94e8261
Carlos Chacin <chacin.carlos9798@gmail.com>
2018-02-21 17:52:09 -0500

    Added animate.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import logo from '../logo_black_green.svg';
-import '../styles/Logo_effect.css';
+import '../styles/Home.css'
+import '../styles/animate.css'
 
 //Renders Home Screen
 class Home extends React.Component {
+  //On Component Home Loaded
+  componentDidMount() {
+
+  }
+
+  //Component Content
   render() {
-     return <img id='logo' alt='Joule Logo' src={logo} />
+    return (
+      // Joule Logo
+      <div className='animated slideInDown' id='logo-container'>
+        <a href='./'>
+          <img id='logo' alt='Joule Logo' src={logo} />
+        </a>
+      </div>
+    )
   }
 }
+
 export default Home;
